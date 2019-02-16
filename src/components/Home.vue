a5601eb56eca3ffb829584b079189ff65adac5f1
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-02-16 16:59:04 +0900

    Add Vue's template codes for Google authentication

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="home">
+    <h1>
+      {{ msg }}
+    </h1>
+    <button>
+      Google アカウント でログインする
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      msg: "😁 Welcome to SPA by Vue.js and Firebase 😁"
+    }
+  }
+}
+</script>
+
