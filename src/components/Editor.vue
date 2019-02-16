b590443be33e7df5a089cff6673f57c881699a7b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-02-16 19:43:29 +0900

    Implement Makrdown Editor (Only one editor)

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="editor">
     <h1>
-      エディター画面
+      エディタ画面
     </h1>
     <span>
       {{ user.displayName }}
@@ -9,20 +9,49 @@
     <button @click="logout">
       ログアウト
     </button>
+
+    <div class="editorWrapper">
+      <textarea class="markdown" v-model="markdown"></textarea>
+      <div class="preview" v-html="preview()"></div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import marked from "marked"
+
 export default {
   name: "editor",
   props: ["user"],
   data() {
-    return {}
+    return {
+      markdown: ""
+    }
   },
   methods: {
     logout: function() {
       firebase.auth().signOut()
+    },
+    preview: function() {
+      return marked(this.markdown)
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.editorWrapper {
+  display: flex;
+}
+
+.markdown {
+  width: 50%;
+  height: 500px;
+}
+
+.preview {
+  width: 50%;
+  text-align: left;
+}
+</style>
