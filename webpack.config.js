65f204090d9b41f0e03aca3770e191d37b8f917d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-02-17 13:47:17 +0900

    Update the target files to apply some style-loader files

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.(sa|sc|c)ss$/,
         use: [
           'vue-style-loader',
           'css-loader'
